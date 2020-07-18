95d67b602dc772975e0529e7f52ab288e2722fbc
HwangSunTae <pozafly@gmail.com>
2020-07-18 16:49:37 +0900

    : function 삭제. ES6 적용

--- src/App.vue
+++ src/App.vue
@@ -18,35 +18,35 @@ import TodoList from './components/TodoList.vue';
 import TodoFooter from './components/TodoFooter.vue';
 
 export default {
-  data: function() {
+  data() {
     return {
       todoItems: [],
     }
   },
   methods: {
-    addOneItem: function(todoItem) {
-      var obj = {completed: false, item: todoItem};
+    addOneItem(todoItem) {
+      const obj = { completed: false, item: todoItem };
       localStorage.setItem(todoItem, JSON.stringify(obj));
       this.todoItems.push(obj);
     },
-    removeOneItem: function(todoItem, index) {
+    removeOneItem(todoItem, index) {
       localStorage.removeItem(todoItem.item);
       this.todoItems.splice(index, 1);
     },
-    toggleOneItem: function(todoItem, index) {
+    toggleOneItem(todoItem, index) {
       this.todoItems[index].completed = !this.todoItems[index].completed;
       // 로컬 스토리지의 데이터를 갱신
       localStorage.removeItem(todoItem.item);
       localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
     },
-    clearAllItems: function() {
+    clearAllItems() {
       localStorage.clear();
       this.todoItems = [];
     }
   },
-  created: function() {
+  created() {
     if(localStorage.length > 0) {
-      for(var i = 0; i < localStorage.length; i++) {
+      for(let i = 0; i < localStorage.length; i++) {
         if(localStorage.key(i) !== 'loglevel:webpack-dev-server') {
             this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
             //this.todoItems.push(localStorage.key(i));
