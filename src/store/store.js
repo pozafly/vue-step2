93dcea19e25897933bba8c2d41a97b11f8eee4c1
HwangSunTae <pozafly@gmail.com>
2020-07-19 17:17:11 +0900

    모듈화1

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import * as getters from './getters'
+import * as mutations from './mutations'
 
 Vue.use(Vuex);
 
@@ -21,30 +23,6 @@ export const store = new Vuex.Store({
     state: {
         todoItems: storage.fetch()
     },
-    getters: {
-        storedTodoItems(state) {
-            return state.todoItems;
-        }
-    },
-    mutations: {
-        addOneItem(state, todoItem) {
-            const obj = { completed: false, item: todoItem };
-            localStorage.setItem(todoItem, JSON.stringify(obj));
-            state.todoItems.push(obj);
-        },
-        removeOneItem(state, { todoItem, index }) {
-            localStorage.removeItem(todoItem.item);
-            state.todoItems.splice(index, 1);
-        },
-        toggleOneItem(state, { todoItem, index }) {
-            state.todoItems[index].completed = !state.todoItems[index].completed;
-            // 로컬 스토리지의 데이터를 갱신
-            localStorage.removeItem(todoItem.item);
-            localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
-        },
-        clearAllItems(state) {
-            localStorage.clear();
-            state.todoItems = [];
-        }
-    }
+    getters,
+    mutations
 });
